75d7e9c70880513bf2d0992d5a0eb9206fd26d0e
Navid Faridi <navid.faridi@gmail.com>
2023-11-13 17:07:24 +0330

    add apiVersioning

---
+++ src/Pargoon.ApiVersioning/ServiceCollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Pargoon.ApiVersioning;
+
+public static class ServiceCollectionExtensions
+{
+  public static IServiceCollection AddSwaggerService(this IServiceCollection services)
+  {
+	services.AddTransient<IConfigureOptions<SwaggerGenOptions>, SwaggerConfigOptions>();
+	services.AddSwaggerGen(options =>
+	{
+	  options.DocumentFilter<SwaggerDocumentFilter>();
+	});
+	services.AddApiVersioning(options =>
+	{
+	  options.DefaultApiVersion = new ApiVersion(1, 0);
+	  options.AssumeDefaultVersionWhenUnspecified = true;
+	  options.ReportApiVersions = true;
+	}).AddApiExplorer(options =>
+	{
+	  options.SubstituteApiVersionInUrl = true;
+	});
+	return services;
+  }
+
+  public static void UseSwaggerVersioned(this WebApplication app)
+  {
+	if (app.Environment.IsDevelopment())
+	{
+	  var versionDescProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+	  app.UseSwagger();
+	  app.UseSwaggerUI(options =>
+	  {
+		foreach (var desc in versionDescProvider.ApiVersionDescriptions)
+		{
+		  options.SwaggerEndpoint($"/swagger/{desc.GroupName}/swagger.json", $"{app.Environment.ApplicationName} - {desc.GroupName.ToUpper()}");
+		}
+	  });
+	}
+  }
+}
---
+++ src/Pargoon.ApiVersioning/SwaggerConfigOptions.cs
@@ -0,0 +1,29 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Pargoon.ApiVersioning;
+
+public class SwaggerConfigOptions : IConfigureOptions<SwaggerGenOptions>
+{
+  private readonly IApiVersionDescriptionProvider _apiVersionDescriptionProvider;
+
+  public SwaggerConfigOptions(IApiVersionDescriptionProvider apiVersionDescriptionProvider)
+  {
+	_apiVersionDescriptionProvider = apiVersionDescriptionProvider;
+  }
+
+  public void Configure(SwaggerGenOptions options)
+  {
+	foreach (var desc in _apiVersionDescriptionProvider.ApiVersionDescriptions)
+	{
+	  options.SwaggerDoc(desc.GroupName, new OpenApiInfo
+	  {
+		Title = $"BookMe API {desc.ApiVersion}",
+		Version = desc.ApiVersion.ToString()
+	  });
+	}
+  }
+}
