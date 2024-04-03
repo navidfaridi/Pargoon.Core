65025f1b9d71b2c0be3989d7b2e5c9b230ba1f92
Navid Faridi <navid.faridi@gmail.com>
2024-04-03 12:39:10 +0330

    adding Pargoon Core library with Result and TResult

---
+++ src/Pargoon.Core/Result.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+
+namespace Pargoon.Core
+{
+	public class PocoResult
+	{
+		public HttpStatusCode Code { get; set; }
+		public string? Description { get; set; }
+
+	}
+	public class Result : PocoResult
+	{
+		public Result()
+		{
+		}
+		public Result(int code)
+		{
+			Code = (HttpStatusCode)code;
+			Success = code == 200;
+		}
+		public Result(HttpStatusCode code)
+		{
+			Code = code;
+			Success = code == HttpStatusCode.OK;
+		}
+		public Result(int code, string? description) : this(code)
+		{
+			Description = description;
+		}
+		public Result(HttpStatusCode code, string? description) : this(code)
+		{
+			Description = description;
+		}
+		public Result(Result result) : this(result.Code, result.Description) { }
+		public bool Success { get; set; }
+	}
+
+	public class BadRequestDataResult : Result
+	{
+		public BadRequestDataResult(string message) : base(HttpStatusCode.BadRequest, message) { }
+	}
+	public class UnauthorizedDataResult : Result
+	{
+		public UnauthorizedDataResult(string message) : base(HttpStatusCode.Unauthorized, message) { }
+	}
+	public class ForbiddenDataResult : Result
+	{
+		public ForbiddenDataResult(string message) : base(HttpStatusCode.Forbidden, message) { }
+	}
+
+	public class NotFoundDataResult : Result
+	{
+		public NotFoundDataResult(string message) : base(HttpStatusCode.NotFound, message) { }
+	}
+
+	public class InternalServerErrorResult : Result
+	{
+		public InternalServerErrorResult(string message) : base(HttpStatusCode.InternalServerError, message) { }
+	}
+}
---
+++ src/Pargoon.Core/TResult.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+namespace Pargoon.Core
+{
+	public class TResult<T> : Result where T : class
+	{
+		public TResult() { }
+		public TResult(int code) : base(code) { }
+		public TResult(HttpStatusCode code) : base(code) { }
+		public TResult(int code, string? description) : base(code, description) { }
+		public TResult(HttpStatusCode code, string? description) : base(code, description) { }
+		public TResult(int code, string? description, T? data) : base(code, description)
+		{
+			Data = data;
+		}
+		public TResult(HttpStatusCode code, string? description, T? data) : base(code, description)
+		{
+			Data = data;
+		}
+
+		public TResult(string? description, T? data) : base(200, description)
+		{
+			Data = data;
+		}
+		public T? Data { get; set; }
+	}
+}
