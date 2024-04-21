03dc554697a2df6daac2189bf6a3d93fec8d700a
Navid Faridi <navid.faridi@gmail.com>
2024-04-21 18:36:02 +0330

    add Pargoon.Core.JWT for dotnet 8

---
+++ src/Pargoon.Core.JWT/JwtConfig.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Pargoon.Core.JWT;
+
+
+public static class JwtConfig
+{
+	static JwtSettings GetJwtSettings(IConfiguration configuration)
+	{
+		return new JwtSettings
+		{
+			Audience = configuration.GetSection("JwtSettings:Audience").Value,
+			Issuer = configuration.GetSection("JwtSettings:Issuer").Value,
+			EncKey = configuration.GetSection("JwtSettings:EncKey").Value,
+			Secret = configuration.GetSection("JwtSettings:Secret").Value
+		};
+	}
+
+	public static void ConfigureJwt(this IServiceCollection services)
+	{
+		var settings = services.BuildServiceProvider().GetRequiredService<IOptions<JwtSettings>>();
+		var config = settings.Value;
+		var isk = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.Secret));
+		var tdk = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.EncKey));
+		//AsymmetricSecurityKey
+		//X509SecurityKey
+		//SecurityKeyIdentifier 
+		//JsonWebKey 
+		var tvp = new TokenValidationParameters
+		{
+			ValidateIssuer = true,
+			ValidateAudience = true,
+			ValidateLifetime = true,
+			ValidateIssuerSigningKey = true,
+
+			ValidIssuer = config.Issuer,
+			ValidAudience = config.Audience,
+			IssuerSigningKey = isk,
+			TokenDecryptionKey = tdk,
+			ClockSkew = TimeSpan.FromMinutes(0)
+		};
+		services
+			.AddAuthentication(o =>
+			{
+				o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+			.AddJwtBearer(opt =>
+			{
+				opt.TokenValidationParameters = tvp;
+			});
+	}
+	public static string CreateToken(string username, Guid userUniqueId, List<string> roles, JwtSettings settings)
+	{
+		List<Claim> claims = new() {
+					new Claim(ClaimTypes.Name, username),
+					new Claim(ClaimTypes.NameIdentifier, userUniqueId.ToString()),
+		};
+		foreach (var item in roles)
+			claims.Add(new Claim(ClaimTypes.Role, item));
+
+		var isk = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.Secret));
+		var tdk = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.EncKey));
+
+		var cred = new SigningCredentials(isk, SecurityAlgorithms.HmacSha256);
+		var encryptingCredentials = new EncryptingCredentials(tdk,
+			JwtConstants.DirectKeyUseAlg,
+			SecurityAlgorithms.Aes256CbcHmacSha512);
+
+		var jwtSecurityToken = new JwtSecurityTokenHandler()
+			.CreateJwtSecurityToken(
+				settings.Issuer,
+				settings.Audience,
+				new ClaimsIdentity(claims),
+				notBefore: DateTime.UtcNow,
+				expires: DateTime.UtcNow.AddMinutes(25),
+				issuedAt: DateTime.UtcNow,
+				signingCredentials: cred,
+				encryptingCredentials: encryptingCredentials
+		);
+
+		var jwt = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+		return jwt;
+	}
+	public static string CreateToken(string username, Guid userUniqueId, List<string> roles, IConfiguration configuration)
+	{
+		var config = GetJwtSettings(configuration);
+		return CreateToken(username, userUniqueId, roles, config);
+	}
+}
---
+++ src/Pargoon.Core.JWT/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Pargoon.Core.JWT;
+
+public class JwtSettings
+{
+	public const string SectionName = "JwtSettings";
+	public string Secret { get; set; } = null!;
+	public string EncKey { get; set; } = null!;
+	public string Issuer { get; set; } = null!;
+	public string Audience { get; set; } = null!;
+}
