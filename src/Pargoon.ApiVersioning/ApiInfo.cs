724116beba94c8dfc7844d4197635b5646d16889
Navid Faridi <navid.faridi@gmail.com>
2024-07-12 22:33:31 +0330

    add api version string

---
+++ src/Pargoon.ApiVersioning/ApiInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Pargoon.ApiVersioning;
+
+public class ApiInfo
+{
+	public const string SectionName = "ApiInfo";
+	public string ApiVersion { get; set; } = string.Empty;
+	public string ApiDescription { get; set; } = string.Empty;
+}
--- src/Pargoon.ApiVersioning/ServiceCollectionExtensions.cs
+++ src/Pargoon.ApiVersioning/ServiceCollectionExtensions.cs
@@ -1,47 +1,58 @@
 ﻿using Asp.Versioning;
 using Asp.Versioning.ApiExplorer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Pargoon.ApiVersioning;
 
 public static class ServiceCollectionExtensions
 {
-  public static IServiceCollection AddSwaggerService(this IServiceCollection services)
-  {
-	services.AddTransient<IConfigureOptions<SwaggerGenOptions>, SwaggerConfigOptions>();
-	services.AddSwaggerGen(options =>
+	public static IServiceCollection AddSwaggerService(this IServiceCollection services, IConfiguration configuration)
 	{
-	  options.DocumentFilter<SwaggerDocumentFilter>();
-	});
-	services.AddApiVersioning(options =>
-	{
-	  options.DefaultApiVersion = new ApiVersion(1, 0);
-	  options.AssumeDefaultVersionWhenUnspecified = true;
-	  options.ReportApiVersions = true;
-	}).AddApiExplorer(options =>
-	{
-	  options.SubstituteApiVersionInUrl = true;
-	});
-	return services;
-  }
+		services.AddTransient<IConfigureOptions<SwaggerGenOptions>, SwaggerConfigOptions>();
+		var version = "1.0.0.0";
+		if (configuration.GetSection(ApiInfo.SectionName) != null)
+			version = configuration[$"{ApiInfo.SectionName}:{nameof(ApiInfo.ApiVersion)}"];
 
-  public static void UseSwaggerVersioned(this WebApplication app)
-  {
-	if (app.Environment.IsDevelopment())
+		var desc = "";
+		if (configuration.GetSection(ApiInfo.SectionName) != null)
+			desc = configuration[$"{ApiInfo.SectionName}:{nameof(ApiInfo.ApiDescription)}"];
+
+		services.AddSwaggerGen(options =>
 	{
-	  var versionDescProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
-	  app.UseSwagger();
-	  app.UseSwaggerUI(options =>
-	  {
-		foreach (var desc in versionDescProvider.ApiVersionDescriptions)
+		options.SwaggerDoc("Version1", new OpenApiInfo { Title = version, Version = "Version1", Description = desc });
+		options.DocumentFilter<SwaggerDocumentFilter>();
+	});
+		services.AddApiVersioning(options =>
 		{
-		  options.SwaggerEndpoint($"/swagger/{desc.GroupName}/swagger.json", $"{app.Environment.ApplicationName} - {desc.GroupName.ToUpper()}");
-		}
-	  });
+			options.DefaultApiVersion = new ApiVersion(1, 0);
+			options.AssumeDefaultVersionWhenUnspecified = true;
+			options.ReportApiVersions = true;
+		}).AddApiExplorer(options =>
+		{
+			options.SubstituteApiVersionInUrl = true;
+		});
+		return services;
 	}
-  }
+
+	public static void UseSwaggerVersioned(this WebApplication app)
+	{
+		if (app.Environment.IsDevelopment())
+		{
+			var versionDescProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+			app.UseSwagger();
+			app.UseSwaggerUI(options =>
+			{
+				foreach (var desc in versionDescProvider.ApiVersionDescriptions)
+				{
+					options.SwaggerEndpoint($"/swagger/{desc.GroupName}/swagger.json", $"{app.Environment.ApplicationName} - {desc.GroupName.ToUpper()}");
+				}
+			});
+		}
+	}
 }
