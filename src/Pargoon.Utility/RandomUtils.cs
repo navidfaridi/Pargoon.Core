8de8383477b560c048c77e12c0468908bc8e933d
Navid Faridi <navid.faridi@outlook.com>
2024-07-20 13:25:53 +0330

    add random password generator

--- src/Pargoon.Utility/RandomUtils.cs
+++ src/Pargoon.Utility/RandomUtils.cs
@@ -2,55 +2,66 @@
 using System.Linq;
 using System.Text;
 
-namespace Pargoon.Utility
+namespace Pargoon.Utility;
+
+public class RandomUtils
 {
-    public class RandomUtils
+    static Random random = new Random();
+    public static int RandomNumber(int min, int max)
     {
-        static Random random = new Random();
-        public static int RandomNumber(int min, int max)
+        return random.Next(min, max);
+    }
+    static string randomStringRange = "abcdefghjkmnpqrstuvwxyz23456789";
+    static string randomPasswordRange = "abcdefghjkmnpqrstuvwxyzABCDEFGHJLKMNPQRSTUVWXYZ23456789";
+    public static string RandomString(int len = 6)
+    {
+        string res = string.Empty;
+        while (res.Length < len)
         {
-            return random.Next(min, max);
+            var r = RandomNumber(0, randomStringRange.Length);
+            res += randomStringRange[r];
         }
-        static string randomStringRange = "abcdefghijkmnpqrstuvwxyz23456789";
-        public static string RandomString(int len = 6)
-        {
-            string res = string.Empty;
-            while (res.Length < len)
-            {
-                var r = RandomNumber(0, randomStringRange.Length);
-                res += randomStringRange[r];
-            }
-            return res;
-        }
+        return res;
+    }
 
-        public static string RandomString(int size, bool lowerCase)
+    public static string RandomPassword(int len = 6)
+    {
+        string res = string.Empty;
+        while (res.Length < len)
         {
-            StringBuilder builder = new StringBuilder();
-            var dontwantChars = "iIloO0uvVU".ToArray();
-            char ch;
-            for (int i = 0; i < size; i++)
-            {
+            var r = RandomNumber(0, randomPasswordRange.Length);
+            res += randomPasswordRange[r];
+        }
+        return res;
+    }
+
+    public static string RandomString(int size, bool lowerCase)
+    {
+        StringBuilder builder = new StringBuilder();
+        var dontwantChars = "iIloO0uvVU".ToArray();
+        char ch;
+        for (int i = 0; i < size; i++)
+        {
+            ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+            while (dontwantChars.Contains(ch))
                 ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
-                while (dontwantChars.Contains(ch))
-                    ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
-                builder.Append(ch);
-            }
-            if (lowerCase)
-                return builder.ToString().ToLower();
-
-            return builder.ToString().ToUpper();
+            builder.Append(ch);
         }
+        if (lowerCase)
+            return builder.ToString().ToLower();
 
-        public static string GenerateRandomCode(int len = 6)
+        return builder.ToString().ToUpper();
+    }
+
+    public static string GenerateRandomCode(int len = 6)
+    {
+        string s = "";
+        while (s.Length < len)
         {
-            string s = "";
-            while (s.Length < len)
-            {
-                int tas = RandomNumber(0, 10);
-                if (tas > 0 && tas < 10)
-                    s = s + tas.ToString();
-            }
-            return s;
+            int tas = RandomNumber(0, 10);
+            if (tas > 0 && tas < 10)
+                s = s + tas.ToString();
         }
+        return s;
     }
 }
