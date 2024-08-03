1629372ebd709efe0d36258d7f4ff12a49bd7a2c
Navid Faridi <navid.faridi@outlook.com>
2024-08-03 11:40:45 +0330

    add jwtSetting configuration in ConfigureJWT add configurable token expire time in minutes in appsettings params

--- src/Pargoon.Core.JWT/JwtConfig.cs
+++ src/Pargoon.Core.JWT/JwtConfig.cs
@@ -12,87 +12,91 @@ namespace Pargoon.Core.JWT;
 
 public static class JwtConfig
 {
-	static JwtSettings GetJwtSettings(IConfiguration configuration)
-	{
-		return new JwtSettings
-		{
-			Audience = configuration.GetSection("JwtSettings:Audience").Value,
-			Issuer = configuration.GetSection("JwtSettings:Issuer").Value,
-			EncKey = configuration.GetSection("JwtSettings:EncKey").Value,
-			Secret = configuration.GetSection("JwtSettings:Secret").Value
-		};
-	}
+    static JwtSettings GetJwtSettings(IConfiguration configuration)
+    {
+        return new JwtSettings
+        {
+            Audience = configuration.GetSection("JwtSettings:Audience").Value ?? "DefaultAudience",
+            Issuer = configuration.GetSection("JwtSettings:Issuer").Value ?? "DefaultIssuer",
+            EncKey = configuration.GetSection("JwtSettings:EncKey").Value ?? "DefaultEncKey",
+            Secret = configuration.GetSection("JwtSettings:Secret").Value ?? "DefaultSecret",
+            ExpirationInMinutes = int.TryParse(configuration.GetSection("JwtSettings:ExpirationInMinutes").Value, out int expiration) ? expiration : 60
+        };
+    }
 
-	public static void ConfigureJwt(this IServiceCollection services)
-	{
-		var settings = services.BuildServiceProvider().GetRequiredService<IOptions<JwtSettings>>();
-		var config = settings.Value;
-		var isk = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.Secret));
-		var tdk = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.EncKey));
-		//AsymmetricSecurityKey
-		//X509SecurityKey
-		//SecurityKeyIdentifier 
-		//JsonWebKey 
-		var tvp = new TokenValidationParameters
-		{
-			ValidateIssuer = true,
-			ValidateAudience = true,
-			ValidateLifetime = true,
-			ValidateIssuerSigningKey = true,
+    public static void ConfigureJwt(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
 
-			ValidIssuer = config.Issuer,
-			ValidAudience = config.Audience,
-			IssuerSigningKey = isk,
-			TokenDecryptionKey = tdk,
-			ClockSkew = TimeSpan.FromMinutes(0)
-		};
-		services
-			.AddAuthentication(o =>
-			{
-				o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-				o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-			})
-			.AddJwtBearer(opt =>
-			{
-				opt.TokenValidationParameters = tvp;
-			});
-	}
-	public static string CreateToken(string username, Guid userUniqueId, List<string> roles, JwtSettings settings)
-	{
-		List<Claim> claims = new() {
-					new Claim(ClaimTypes.Name, username),
-					new Claim(ClaimTypes.NameIdentifier, userUniqueId.ToString()),
-		};
-		foreach (var item in roles)
-			claims.Add(new Claim(ClaimTypes.Role, item));
+        var settings = services.BuildServiceProvider().GetRequiredService<IOptions<JwtSettings>>();
+        var config = settings.Value;
 
-		var isk = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.Secret));
-		var tdk = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.EncKey));
+        var isk = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.Secret));
+        var tdk = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.EncKey));
+        //AsymmetricSecurityKey
+        //X509SecurityKey
+        //SecurityKeyIdentifier 
+        //JsonWebKey 
+        var tvp = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
 
-		var cred = new SigningCredentials(isk, SecurityAlgorithms.HmacSha256);
-		var encryptingCredentials = new EncryptingCredentials(tdk,
-			JwtConstants.DirectKeyUseAlg,
-			SecurityAlgorithms.Aes256CbcHmacSha512);
+            ValidIssuer = config.Issuer,
+            ValidAudience = config.Audience,
+            IssuerSigningKey = isk,
+            TokenDecryptionKey = tdk,
+            ClockSkew = TimeSpan.FromMinutes(0)
+        };
+        services
+            .AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(opt =>
+            {
+                opt.TokenValidationParameters = tvp;
+            });
+    }
+    public static string CreateToken(string username, Guid userUniqueId, List<string> roles, JwtSettings settings)
+    {
+        List<Claim> claims = new() {
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim(ClaimTypes.NameIdentifier, userUniqueId.ToString()),
+        };
+        foreach (var item in roles)
+            claims.Add(new Claim(ClaimTypes.Role, item));
 
-		var jwtSecurityToken = new JwtSecurityTokenHandler()
-			.CreateJwtSecurityToken(
-				settings.Issuer,
-				settings.Audience,
-				new ClaimsIdentity(claims),
-				notBefore: DateTime.UtcNow,
-				expires: DateTime.UtcNow.AddMinutes(25),
-				issuedAt: DateTime.UtcNow,
-				signingCredentials: cred,
-				encryptingCredentials: encryptingCredentials
-		);
+        var isk = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.Secret));
+        var tdk = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.EncKey));
 
-		var jwt = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+        var cred = new SigningCredentials(isk, SecurityAlgorithms.HmacSha256);
+        var encryptingCredentials = new EncryptingCredentials(tdk,
+            JwtConstants.DirectKeyUseAlg,
+            SecurityAlgorithms.Aes256CbcHmacSha512);
 
-		return jwt;
-	}
-	public static string CreateToken(string username, Guid userUniqueId, List<string> roles, IConfiguration configuration)
-	{
-		var config = GetJwtSettings(configuration);
-		return CreateToken(username, userUniqueId, roles, config);
-	}
+        var jwtSecurityToken = new JwtSecurityTokenHandler()
+            .CreateJwtSecurityToken(
+                settings.Issuer,
+                settings.Audience,
+                new ClaimsIdentity(claims),
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddMinutes(settings.ExpirationInMinutes),
+                issuedAt: DateTime.UtcNow,
+                signingCredentials: cred,
+                encryptingCredentials: encryptingCredentials
+        );
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+        return jwt;
+    }
+    public static string CreateToken(string username, Guid userUniqueId, List<string> roles, IConfiguration configuration)
+    {
+        var config = GetJwtSettings(configuration);
+        return CreateToken(username, userUniqueId, roles, config);
+    }
 }
--- src/Pargoon.Core.JWT/JwtSettings.cs
+++ src/Pargoon.Core.JWT/JwtSettings.cs
@@ -2,9 +2,10 @@
 
 public class JwtSettings
 {
-	public const string SectionName = "JwtSettings";
-	public string Secret { get; set; } = null!;
-	public string EncKey { get; set; } = null!;
-	public string Issuer { get; set; } = null!;
-	public string Audience { get; set; } = null!;
+    public const string SectionName = "JwtSettings";
+    public string Secret { get; set; } = null!;
+    public string EncKey { get; set; } = null!;
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+    public int ExpirationInMinutes { get; set; } = 25;
 }
