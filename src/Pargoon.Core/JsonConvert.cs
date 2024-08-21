06773f2db7ff5650fc394f7bb2fe2d70c00d06e1
Navid Faridi <navid.faridi@gmail.com>
2024-08-21 09:56:25 +0330

    add JsonConvert with default JsonSerializerOptions Unsafe , CamelCase and CaseInsensitive

---
+++ src/Pargoon.Core/JsonConvert.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace Pargoon.Core;
+
+public class JsonConvert
+{
+	public static JsonSerializerOptions UnsafeCamelCase
+	{
+		get
+		{
+			return new JsonSerializerOptions
+			{
+				Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+				PropertyNameCaseInsensitive = true
+			};
+		}
+	}
+	public static string Serialize(object obj, JsonSerializerOptions? options = null)
+	{
+		return JsonSerializer.Serialize(obj, options ?? JsonConvert.UnsafeCamelCase);
+	}
+
+	public static T? Deserialize<T>(string txt, JsonSerializerOptions? options = null)
+	{
+		return JsonSerializer.Deserialize<T>(txt, options ?? JsonConvert.UnsafeCamelCase);
+	}
+}
