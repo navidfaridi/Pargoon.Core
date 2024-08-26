7af63e1771f6b8de1a69bd599537c8b9fd9981fc
Navid Faridi <navid.faridi@gmail.com>
2024-08-26 17:31:16 +0330

    update pargoon extension linq

--- src/Pargoon.Extensions.Linq/TPaginationResult.cs
+++ src/Pargoon.Extensions.Linq/TPaginationResult.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections.Generic;
-using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 
 namespace Pargoon.Extensions.Linq;
 
 public class TPaginationResult<T>
 {
-    [DataMember(Name = "data")]
     [JsonPropertyName("data")]
     public List<T> Data { get; set; } = new List<T>();
 
     [JsonPropertyName("pageSize")]
     public int PageSize { get; set; } = 10;
+
+    [JsonPropertyName("pageIndex")]
+    public int PageIndex { get; set; } = 0;
 
     [JsonPropertyName("pageCount")]
     public int PageCount
