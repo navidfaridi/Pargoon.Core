f06c14485ca9759eceeecf07efe160a3aeefd5c2
Navid Faridi <navid.faridi@gmail.com>
2024-09-07 22:51:50 +0330

    add new Fail extension for TResult with AppError class parameter

--- src/Pargoon.Core/TResult.cs
+++ src/Pargoon.Core/TResult.cs
@@ -7,97 +7,97 @@ namespace Pargoon.Core;
 [Serializable]
 public class PocoResult
 {
-	[DataMember(Name = "statusCode")]
-	public HttpStatusCode StatusCode { get; set; }
-	[DataMember(Name = "code")]
-	public int Code { get; set; }
-	[DataMember(Name = "description")]
-	public string? Description { get; set; }
+    [DataMember(Name = "statusCode")]
+    public HttpStatusCode StatusCode { get; set; }
+    [DataMember(Name = "code")]
+    public int Code { get; set; }
+    [DataMember(Name = "description")]
+    public string? Description { get; set; }
 
 }
 public class TResult : PocoResult
 {
-	public TResult()
-	{
-	}
-	public TResult(int code)
-	{
-		Code = code;
-		StatusCode = (HttpStatusCode)code;
-	}
-	public TResult(HttpStatusCode statusCode)
-	{
-		StatusCode = statusCode;
-		Code = (int)statusCode;
-	}
-	public TResult(int code, string? description) : this(code)
-	{
-		Description = description;
-	}
-	public TResult(HttpStatusCode code, string? description) : this(code)
-	{
-		Description = description;
-	}
+    public TResult()
+    {
+    }
+    public TResult(int code)
+    {
+        Code = code;
+        StatusCode = (HttpStatusCode)code;
+    }
+    public TResult(HttpStatusCode statusCode)
+    {
+        StatusCode = statusCode;
+        Code = (int)statusCode;
+    }
+    public TResult(int code, string? description) : this(code)
+    {
+        Description = description;
+    }
+    public TResult(HttpStatusCode code, string? description) : this(code)
+    {
+        Description = description;
+    }
 
-	public bool IsSuccess { get { return Code == 200 || StatusCode == HttpStatusCode.OK; } }
+    public bool IsSuccess { get { return Code == 200 || StatusCode == HttpStatusCode.OK; } }
 }
 
 public class BadRequestDataResult : TResult
 {
-	public BadRequestDataResult(string message) : base(HttpStatusCode.BadRequest, message) { }
+    public BadRequestDataResult(string message) : base(HttpStatusCode.BadRequest, message) { }
 }
 public class UnauthorizedDataResult : TResult
 {
-	public UnauthorizedDataResult(string message) : base(HttpStatusCode.Unauthorized, message) { }
+    public UnauthorizedDataResult(string message) : base(HttpStatusCode.Unauthorized, message) { }
 }
 public class ForbiddenDataResult : TResult
 {
-	public ForbiddenDataResult(string message) : base(HttpStatusCode.Forbidden, message) { }
+    public ForbiddenDataResult(string message) : base(HttpStatusCode.Forbidden, message) { }
 }
 
 public class NotFoundDataResult : TResult
 {
-	public NotFoundDataResult(string message) : base(HttpStatusCode.NotFound, message) { }
+    public NotFoundDataResult(string message) : base(HttpStatusCode.NotFound, message) { }
 }
 
 public class InternalServerErrorResult : TResult
 {
-	public InternalServerErrorResult(string message) : base(HttpStatusCode.InternalServerError, message) { }
+    public InternalServerErrorResult(string message) : base(HttpStatusCode.InternalServerError, message) { }
 }
 
 public class TResult<T> : TResult
 {
-	public static TResult<T> Success(T value, HttpStatusCode statusCode = HttpStatusCode.OK, string description = "")
-	{
-		return new TResult<T>(statusCode, description, value);
-	}
+    public static TResult<T> Success(T value, HttpStatusCode statusCode = HttpStatusCode.OK, string description = "")
+    {
+        return new TResult<T>(statusCode, description, value);
+    }
 
-	public static TResult<T> Fail(int code, string? description)
-	{
-		return new TResult<T>(code, description);
-	}
-	public static TResult<T> Fail(HttpStatusCode statucCode, string? description)
-	{
-		return new TResult<T>(statucCode, description);
-	}
-	public TResult() { }
-	public TResult(int code) : base(code) { }
-	public TResult(HttpStatusCode statusCode) : base(statusCode) { }
-	public TResult(int code, string? description) : base(code, description) { }
-	public TResult(HttpStatusCode statusCode, string? description) : base(statusCode, description) { }
-	public TResult(int code, string? description, T? data) : base(code, description)
-	{
-		Data = data;
-	}
-	public TResult(HttpStatusCode statusCode, string? description, T? data) : base(statusCode, description)
-	{
-		Data = data;
-	}
+    public static TResult<T> Fail(int code, string? description)
+    {
+        return new TResult<T>(code, description);
+    }
+    public static TResult<T> Fail(HttpStatusCode statucCode, string? description)
+    {
+        return new TResult<T>(statucCode, description);
+    }
+    public TResult() { }
+    public TResult(int code) : base(code) { }
+    public TResult(HttpStatusCode statusCode) : base(statusCode) { }
+    public TResult(int code, string? description) : base(code, description) { }
+    public TResult(HttpStatusCode statusCode, string? description) : base(statusCode, description) { }
+    public TResult(int code, string? description, T? data) : base(code, description)
+    {
+        Data = data;
+    }
+    public TResult(HttpStatusCode statusCode, string? description, T? data) : base(statusCode, description)
+    {
+        Data = data;
+    }
 
-	public TResult(string? description, T? data) : base(200, description)
-	{
-		Data = data;
-	}
-	[DataMember(Name = "data")]
-	public T? Data { get; set; }
+    public TResult(string? description, T? data) : base(200, description)
+    {
+        Data = data;
+    }
+    [DataMember(Name = "data")]
+    public T? Data { get; set; }
 }
--- src/Pargoon.Exceptions/AppError.cs
+++ src/Pargoon.Exceptions/AppError.cs
@@ -1,13 +1,23 @@
-﻿using System.Net;
+﻿using Pargoon.Core;
+using System.Net;
 
 namespace Pargoon.Exceptions;
 public class AppError
 {
-	public AppError()
-	{
+    public AppError()
+    {
 
-	}
-	public HttpStatusCode StatusCode { get; set; }
-	public string ErrorCode { get; set; } = null!;
-	public string ErrorMessage { get; set; } = null!;
+    }
+    public HttpStatusCode StatusCode { get; set; }
+    public string ErrorCode { get; set; } = null!;
+    public string ErrorMessage { get; set; } = null!;
+}
+
+
+public static class TResultExtensions
+{
+    public static TResult<T> Fail<T>(AppError error)
+    {
+        return new TResult<T>(error.StatusCode, $"({error.ErrorCode}): {error.ErrorMessage}");
+    }
 }
