560f3cede6385c1e4953a846fec4f8b9b8b35389
Navid Faridi <navid.faridi@gmail.com>
2024-09-28 12:49:17 +0330

    add ReferenceHandler = ReferenceHandler.IgnoreCycles to json serialization option

--- src/Pargoon.Core/JsonConvert.cs
+++ src/Pargoon.Core/JsonConvert.cs
@@ -1,29 +1,31 @@
 ﻿using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Pargoon.Core;
 
 public class JsonConvert
 {
-	public static JsonSerializerOptions UnsafeCamelCase
-	{
-		get
-		{
-			return new JsonSerializerOptions
-			{
-				Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-				PropertyNameCaseInsensitive = true
-			};
-		}
-	}
-	public static string Serialize(object obj, JsonSerializerOptions? options = null)
-	{
-		return JsonSerializer.Serialize(obj, options ?? JsonConvert.UnsafeCamelCase);
-	}
+    public static JsonSerializerOptions UnsafeCamelCase
+    {
+        get
+        {
+            return new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                PropertyNameCaseInsensitive = true,
+                ReferenceHandler = ReferenceHandler.IgnoreCycles
+            };
+        }
+    }
+    public static string Serialize(object obj, JsonSerializerOptions? options = null)
+    {
+        return JsonSerializer.Serialize(obj, options ?? JsonConvert.UnsafeCamelCase);
+    }
 
-	public static T? Deserialize<T>(string txt, JsonSerializerOptions? options = null)
-	{
-		return JsonSerializer.Deserialize<T>(txt, options ?? JsonConvert.UnsafeCamelCase);
-	}
+    public static T? Deserialize<T>(string txt, JsonSerializerOptions? options = null)
+    {
+        return JsonSerializer.Deserialize<T>(txt, options ?? JsonConvert.UnsafeCamelCase);
+    }
 }
